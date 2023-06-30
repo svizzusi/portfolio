56f8df4e4fd9a7fc697ffa922f9973c00abf9362
Stephen Vizzusi <svizzusi13@gmail.com>
2023-06-30 11:35:19 -0700

    Added toast for form

--- src/componets/Form/FormComponent.jsx
+++ src/componets/Form/FormComponent.jsx
@@ -1,6 +1,9 @@
 import style from './Form.module.css'
 import emailjs from '@emailjs/browser'
 import React, { useRef } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 const FormComponent = () => {
     const formRef = useRef()
@@ -9,20 +12,40 @@ const FormComponent = () => {
         emailjs.sendForm('service_iznybsu', 'template_ojdq00o', formRef.current, 'If7t9Q2qS_tvKYRZR')
         .then((result) => {
             console.log(result.text)
-            console.log('message sent')
+            toast.success('Message Sent', {
+                position: "top-center",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: false,
+                pauseOnHover: true,
+                draggable: false,
+                progress: undefined,
+                theme: "dark",
+                
+                });
             formRef.current.reset()
+
         }, (error) => {
             console.log(error.text)
-            console.log('Message not sent. Error!')
+            toast.error('Error Sending Message', {
+                position: "top-center",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: false,
+                pauseOnHover: true,
+                draggable: false,
+                progress: undefined,
+                theme: "dark",
+                });
         })
     }
     return (
         <form className={style.formForm} onSubmit={sendEmail} ref={formRef}>
-            <input type='name' name='user_name' placeholder='Name' />
-            <input type='email' name='user_email' placeholder='Email' />
-            <textarea name='user_message' placeholder='Write message...'></textarea>
+            <input type='name' name='user_name' placeholder='Name' required/>
+            <input type='email' name='user_email' placeholder='Email' required/>
+            <textarea name='user_message' placeholder='Write message...' required></textarea>
             <button type='submit'>Submit</button>
-            
+            <ToastContainer />
         </form>
     )
 }
