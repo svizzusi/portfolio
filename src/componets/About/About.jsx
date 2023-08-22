b8b16ea471495b55f0d643474035bbe6ea899e63
Stephen Vizzusi <svizzusi13@gmail.com>
2023-08-22 09:53:51 -0700

    added Resume

--- src/componets/About/About.jsx
+++ src/componets/About/About.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import style from './About.module.css'
-import { FiDownload } from 'react-icons/fi';
+import { FiDownload } from 'react-icons/fi'
 import profilePic from '../../assets/images/profilePic.jpg'
+import Resume from '../../assets/images/StephenVizzusiResume.pdf'
 
 const About = ({aboutSectionRef}) => {
+
+  const downloadFile = () => {
+    const fileUrl = Resume;
+    window.open(fileUrl, "_blank")
+    }
+    
+
   return (
     <div id='aboutSection' ref={aboutSectionRef} className={style.aboutContainer}>
         <section className={style.aboutSection}>
@@ -11,7 +19,13 @@ const About = ({aboutSectionRef}) => {
             <div className={style.aboutText}>
                 <h2>About <span>Me:</span></h2>
                 <p data-aos="zoom-in" >I am a Fullstack Software Engineer from San Jose, CA. I consider myself a creative engineer who is efficient in delivering high-quality products. I am focused on the end-user and in providing the best possible experience when interacting with the UI. I work mostly with JavaScript, React and CSS, but I am also versed in back-end solutions and testing.</p>
-                <button  className={style.aboutButton}><FiDownload className={style.resume}/> Resume</button>
+                <button 
+                  onClick={downloadFile} 
+                  download  
+                  className={style.aboutButton}>
+                <FiDownload className={style.resume}/> 
+                Resume</button>
+                <a href={Resume} download='StephenVizzusiResume'>Download Resume</a>
             </div>
         </section>
     </div>
