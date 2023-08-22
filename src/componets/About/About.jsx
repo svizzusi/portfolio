76bbd8b9dce5293afee236cb75f3b99ad1c49bd4
Stephen Vizzusi <svizzusi13@gmail.com>
2023-08-22 09:55:59 -0700

    Added Resume to Download on a seperate page

--- src/componets/About/About.jsx
+++ src/componets/About/About.jsx
@@ -25,7 +25,6 @@ const About = ({aboutSectionRef}) => {
                   className={style.aboutButton}>
                 <FiDownload className={style.resume}/> 
                 Resume</button>
-                <a href={Resume} download='StephenVizzusiResume'>Download Resume</a>
             </div>
         </section>
     </div>
